b461eb3237f385db0762c245ad2605eeb2a3d838
kabanskiy <yarikkabanskiy@mail.com>
2023-05-18 23:37:16 +0300

    Созданы поля в бд

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
 
+class Baza(db):
+    id = db.Column(db.integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    intro = db.Column(db.String(250), nullable = False)
+    text = db.Column(db.Text, nullable = False)
+    date = db.Column(db.DateTime, default = datetime.utcnow)
 
 @app.route('/')
 @app.route('/home')
